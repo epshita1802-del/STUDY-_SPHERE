176dc44d57f59e5915bf2b7c98b024d1ff6e3cf8
epshita1802-del <epshita1802@gmail.com>
2026-05-08 15:45:06 +0530

    Delete Studysphere directory

--- Studysphere/javascript/syllabus.js
+++
@@ -1,8 +0,0 @@
-function toggleContent(id) {
-    var content = document.getElementById(id);
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-}
